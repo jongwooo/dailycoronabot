7365dde74949e4f1bed6b1837ec4366e40fc5d9f
jongwooo <han980817@gmail.com>
2020-02-27 21:52:06 +0900

    Update bot.js for fix crawler error

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ getHTML()
     let timeList = [];
 
     const $ = cheerio.load(html.data);
-    const $countList = $("ul.s_listin_dot").children("li");
+    const $countList = $("table.num tr").children("td.w_bold");
     const $timeList = $("div.bvc_txt").children("p.s_descript");
 
     moment.tz.setDefault("Asia/Seoul");
@@ -36,15 +36,11 @@ getHTML()
     let ts = t.indexOf("(") + 1;
     let time = t.substring(ts, t.indexOf(")", ts)).replace(".", "월 ");
 
-    let c,
-      cs = null;
     $countList.each(function(i) {
-      c = $(this).text();
-      cs = c.indexOf(")") + 1;
-      countList[i] = c.substring(cs, c.indexOf("명", cs));
+      countList[i] = $(this).text().replace(" 명", "명");
     });
 
-    let content = `${date}(${time}),\n대한민국의 코로나바이러스 현황\n\n[바이러스 확진환자수]${countList[0]}명\n[확진환자 격리해제수]${countList[1]}명\n[국내확진자 사망자수]${countList[2]}명\n[바이러스 검사진행수]${countList[3]}명\n\n#코로나바이러스감염증 #국내확진자`;
+    let content = `${date}(${time}),\n대한민국의 코로나바이러스 현황\n\n[바이러스 확진환자수] ${countList[0]}\n[확진환자 격리해제수] ${countList[1]}\n[국내확진자 사망자수] ${countList[2]}\n[바이러스 검사진행수] ${countList[3]}\n\n#코로나바이러스감염증 #국내확진자`;
 
     return content;
   })
