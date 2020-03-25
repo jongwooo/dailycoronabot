6083cbaa425c3230c06b070e4ad6198eaeff0ede
jongwooo <han980817@gmail.com>
2020-03-25 13:34:27 +0900

    Update bot.js to remove useless code

--- bot.js
+++ bot.js
@@ -21,7 +21,6 @@ getHTML()
 		try {
 			let countList = [];
 			let comparedList = [];
-			let timeList = [];
 
 			const $ = cheerio.load(html.data);
 			const $liveNum = $("ul.liveNum li");
@@ -32,28 +31,24 @@ getHTML()
 			moment.tz.setDefault("Asia/Seoul");
 			const date = moment().format("M월 DD일 HH시");
 
-			$timeList.each(function(i) {
-				timeList[i] = $(this).text();
-			});
-
-			let t = timeList[0];
+			let t = $timeList.text();
 			let ts = t.indexOf("(") + 1;
 			let time = t
 				.substring(ts, t.indexOf(",", ts))
 				.replace(".", "월 ")
 				.replace(".", "일");
 
-			$countList.each(function(j) {
+			$countList.each(function(i) {
 				let countListContent = $(this).text();
 
 				if (countListContent.includes("(누적)")) {
 					countListContent = countListContent.replace("(누적)", "");
 				}
 
-				countList[j] = `${countListContent} 명`;
+				countList[i] = `${countListContent} 명`;
 			});
 
-			$comparedList.each(function(k) {
+			$comparedList.each(function(j) {
 				let comparedListContent = $(this).text();
 
 				if (comparedListContent.includes("전일대비 ")) {
@@ -66,7 +61,7 @@ getHTML()
 					comparedListContent = comparedListContent.replace("- ", "-");
 				}
 
-				comparedList[k] = `${comparedListContent}`;
+				comparedList[j] = `${comparedListContent}`;
 			});
 
 			let content = `${date} (${time}),\n대한민국의 코로나바이러스 현황\n\n[확진환자] ${countList[0]} ${comparedList[0]}\n[격리해제] ${countList[1]} ${comparedList[1]}\n[격리진행] ${countList[2]} ${comparedList[2]}\n[사망자수] ${countList[3]} ${comparedList[3]}\n\n#힘내라_대한민국 #응원해요_의료진\n#코로나바이러스감염증 #국내확진자`;
