59770da05445c5f87e84addb20a544447a2813c1
jongwooo <han980817@gmail.com>
2021-11-11 16:39:00 +0000

    Change the name of the parameters in bot.js

--- bot.js
+++ bot.js
@@ -20,26 +20,29 @@ getHTML()
 	.then(html => {
 		try {
 			const $ = cheerio.load(html.data);
-			const $vaccinePercent = $("div.vaccine_list ul li.percent");
-			const $countList = $("div.occurrenceStatus table.ds_table tbody tr td span").eq(3);
-			const $timeList = $("div.occurrenceStatus h2 span.livedate");
+			const $getCoronaInfo = $(
+				"div.occurrenceStatus table.ds_table tbody tr td span"
+			);
+			const $getTime = $("div.occurrenceStatus h2 span.livedate");
 
-			let firstVaccine = $vaccinePercent.eq(0).text();
-			let secondVaccine = $vaccinePercent.eq(1).text();
-
-			let countList = $countList.text();
+			const todaysDeath = $getCoronaInfo.eq(0).text();
+			const todaysSeverePatient = $getCoronaInfo.eq(1).text();
+			const todaysNormalPatient = $getCoronaInfo.eq(2).text();
+			const todaysCases = $getCoronaInfo.eq(3).text();
 
 			moment.tz.setDefault("Asia/Seoul");
 			const date = moment().format("M월 DD일 HH시");
 
-			let t = $timeList.text();
-			let ts = t.indexOf("(") + 1;
-			let time = t
-				.substring(ts, t.indexOf(",", ts))
+			const rawTime = $getTime.text();
+			const startIndex = rawTime.indexOf("(") + 1;
+			const lastIndex = rawTime.indexOf(",", startIndex);
+
+			let structuredTime = rawTime
+				.substring(startIndex, lastIndex)
 				.replace(".", "월 ")
 				.replace(".", "일");
 
-			let content = `${date} (${time}),\n대한민국의 코로나바이러스 현황\n\n[1차 접종] ${firstVaccine}\n[접종완료] ${secondVaccine}\n[일일확진] ${countList} 명\n\n#위드_코로나 #단계적_일상회복\n#코로나바이러스감염증 #국내확진자`;
+			let content = `${date} (${structuredTime}),\n대한민국의 코로나바이러스 현황\n\n[일일 사망자] ${todaysDeath} 명\n[재원 위중증] ${todaysSeverePatient} 명\n[신규 입원자] ${todaysNormalPatient} 명\n[일일 확진자] ${todaysCases} 명\n\n#위드_코로나 #단계적_일상회복\n#코로나바이러스감염증 #국내확진자`;
 
 			return content;
 		} catch (error) {
@@ -49,11 +52,11 @@ getHTML()
 	})
 	.then(content => tweetPost(content));
 
-const tweetPost = content => {
-	if (content) {
-		client.post("statuses/update", { status: content }, error => {
+const tweetPost = tweet => {
+	if (tweet) {
+		client.post("statuses/update", { status: tweet }, error => {
 			if (!error) {
-				console.log(`tweet success: ${content}`);
+				console.log(`tweet success: ${tweet}`);
 			} else {
 				console.error(`tweet failed: ${error}`);
 			}
